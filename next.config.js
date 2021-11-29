2cf31615fa8eb4bbea1fabd641fc22ecc91384f2
Brhane Giday <35131050+brhanegiday@users.noreply.github.com>
2021-11-29 09:10:24 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  target: "serverless",
+//   target: "serverless",
+  location /_next/webpack-hmr {
+    proxy_pass http://localhost:3000/_next/webpack-hmr;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection "upgrade";
+}
 };
